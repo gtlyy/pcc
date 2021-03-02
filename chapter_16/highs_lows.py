da668c9201ba98d1947926de63d6f5673d4bcbaa
hylyy <514111912@qq.com>
2021-03-02 20:12:19 +0800

    16

--- chapter_16/highs_lows.py
+++ chapter_16/highs_lows.py
@@ -26,14 +26,17 @@ with open(filename) as f:
 fig = plt.figure(dpi=128, figsize=(10, 6))
 plt.plot(dates, highs, c='red', alpha=0.5)
 plt.plot(dates, lows, c='blue', alpha=0.5)
+# 绘制highs、lows中间区域
 plt.fill_between(dates, highs, lows, facecolor='blue', alpha=0.1)
 
 # Format plot.
 title = "Daily high and low temperatures - 2014\nDeath Valley, CA"
 plt.title(title, fontsize=20)
 plt.xlabel('', fontsize=16)
+# 将日期放斜
 fig.autofmt_xdate()
 plt.ylabel("Temperature (F)", fontsize=16)
 plt.tick_params(axis='both', which='major', labelsize=16)
+plt.ylim(10, 120)
 
 plt.show()
